525627f89e6a1de4b599f697164dd87adc79aaf2
jacobhayward <jacobhaywardx@gmail.com>
2023-03-04 09:59:35 +0000

    Safety, Health and environment functions. Allows to search for chemical and tell you the SHE scores and an overall ranking.

--- Correlation.py
+++ Correlation.py
@@ -3,6 +3,7 @@ import pandas as pd
 import plotly.express as px
 from sklearn import impute
 from sklearn import preprocessing
+from itertools import combinations
 import os
 
 if not os.path.exists("Correlations"):
@@ -12,7 +13,6 @@ data = pd.read_csv('Dataset.csv')
 data_descriptors = data.drop(columns=['CHEM21 Solvents', 'Sustainability'])
 
 columns = list(data_descriptors)
-
 imp = impute.SimpleImputer(missing_values=np.nan, strategy='mean')
 imp.fit(data_descriptors)
 data_df_impute = imp.transform(data_descriptors)
@@ -22,11 +22,14 @@ data_df_impute = scaler.transform(data_df_impute)
 
 data_df_impute = pd.DataFrame(data_df_impute, columns=columns)
 
+res = list(combinations(data_descriptors, 2))
 
-for x in columns:
-    for y in columns:
-        if x == y:
-            continue
-    fig = px.scatter(data_df_impute, x=x, y=y)
-    fig.write_image("Correlations/fig.png")
+
+for i in res:
+    x = i[0]
+    y = i[1]
+    fig = px.scatter(data_df_impute, x=i[0], y=i[1])
+
+
+
 
