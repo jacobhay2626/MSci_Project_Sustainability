5d523e769d2a2ae21e8ae5852938c8c08d8b58bd
jacobhayward <jacobhaywardx@gmail.com>
2023-03-13 12:49:46 +0000

    Safety, Health and environment functions. Allows to search for chemical and tell you the SHE scores and an overall ranking.

--- classification.py
+++ classification.py
@@ -8,14 +8,40 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.model_selection import train_test_split, KFold
 from sklearn.neighbors import KNeighborsClassifier
+from tabulate import tabulate
+import xlsxwriter
 
 
 def classification(descriptors, ml_method, validation):
+
+    #  ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    #  READ CSV FILE
     data = pd.read_csv("Dataset.csv")
-    data_descriptors = data[descriptors]
+    d_data = pd.read_csv("Dataset.csv", usecols=['HTP(ingestion)log10', 'HTP(Inhalation)log10', 'XVP',
+                                                 'Boiling Point', 'Resistivitylog10',
+                                                 'Peroxide formation', 'AIT', 'CGPlog10', 'CLP',
+                                                 'Aquatic Toxicity (mg/L)log10'])
+
+    #  ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    #  DEFINE DESCRIPTORS
+    #  CREATED TWO: ONE INCLUDING THE SOLVENTS AND ONE WITHOUT
+
+    string_d_data = data[descriptors]
+    descriptors = ['HTP(ingestion)log10', 'HTP(Inhalation)log10', 'XVP',
+                   'Boiling Point', 'Resistivitylog10',
+                   'Peroxide formation', 'AIT', 'CGPlog10', 'CLP',
+                   'Aquatic Toxicity (mg/L)log10']
+    data_descriptors = d_data[descriptors]
+
     y = data['Sustainability']
 
     columns = list(data_descriptors)
+
+    #  ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    #  MEAN, FIT, TRANSFORM AND SCALE
 
     # All missing values were given the mean value for the column
     imp = impute.SimpleImputer(missing_values=np.nan, strategy='mean')
@@ -29,7 +55,20 @@ def classification(descriptors, ml_method, validation):
     scaler = preprocessing.StandardScaler().fit(data_df_impute)
     data_df_impute = scaler.transform(data_df_impute)
 
+    #  ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    # CREATE DATAFRAMES: ONE FOR WITHOUT SOLVENT NAMES
+    #  ONE DATAFRAME FOR THE SOLVENT NAMES
+
     data_df_impute = pd.DataFrame(data_df_impute, columns=columns)
+    df = pd.DataFrame(string_d_data, columns=columns_b)
+
+    solvents = df[["CHEM21 Solvents"]].values
+    X_solvents = solvents.astype('|S')
+
+    #  ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    #  METHOD, VALIDATION METHOD
 
     # X_train, X_test, y_train, y_test = train_test_split(data_df_impute, y, test_size=0.2)
     if ml_method == "RF":
@@ -44,6 +83,10 @@ def classification(descriptors, ml_method, validation):
 
     kf = KFold(n_splits=k, random_state=None, shuffle=False)
 
+    #  ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    #  MODEL
+
     preds = []
     real = []
 
@@ -55,13 +98,29 @@ def classification(descriptors, ml_method, validation):
         preds.extend(y_pred)
         real.extend(y_test)
 
-    print(preds)
-    print(real)
+    #  ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    #  CONFUSION MATRIX, CLASSIFICATION REPORT
+
+    # table = [['                 Solvents             ', "Real", "Preds"], [X_solvents], [real], [preds]]
+
+    # print(preds)
+    # print(X_solvents)
+    # print(real)
     print("=== Confusion Matrix ===")
     print(confusion_matrix(real, preds))
     print('\n')
     print("=== Classification Report ===")
     print(classification_report(real, preds))
+
+    #  I want a table that shows the solvents, their given score and the score predicted by programme.
+    # Column 1: solvents
+    # Column 2: scores
+    # Column 3: predicted scores
+    # print(tabulate(table))
+    # print(type(X_solvents))
+    # print(tabulate(table, tablefmt='grid'))
+
     return preds, real, confusion_matrix(real, preds), classification_report(real, preds)
 
 # ---------------------------------------------------------------------------------------------
